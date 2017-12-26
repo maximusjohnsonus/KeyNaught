748333f17cd1e63ad5763f60ea94c0882b2e5c0a
Miranda Durham <mdurham@miranda-durhams-imac.local>
2017-12-25 20:34:31 -0700

    Initial Commit

---
+++ KeyNaught/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyNaught
+//
+//  Created by Miranda Durham on 12/25/17.
+//  Copyright © 2017 dOOP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KeyNaught/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KeyNaught
+//
+//  Created by Miranda Durham on 12/25/17.
+//  Copyright © 2017 dOOP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
